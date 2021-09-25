8b459e268d34f2625834f13a70401c42388573b6
Earther <qn.khuat@gmail.com>
2021-09-25 16:08:40 +0700

    add a list of functions to implemenmt

--- src/clojury/core.clj
+++ src/clojury/core.clj
@@ -1,7 +1,78 @@
 (ns clojury.core
   (:gen-class))
 
+; can use as provided:
+; map, filter, cons, conj, apply, rest, loop, for, next, reduce, lazy-seq
+
+;(defn concat [] ())
+
+(defn _map [] ())
+(defn _remove [] ())
+(defn _mapcat [] ())
+(defn _interleave [] ())
+(defn _nthrest [] ())
+(defn _fnext [] ())
+(defn _nnext [] ())
+(defn _drop [] ())
+(defn _drop-last [] ())
+(defn _drop-while [] ())
+(defn _take-last [] ())
+(defn _distinct [] ())
+(defn _partition[] ())
+(defn _split-at [] ())
+(defn _split-with [] ())
+(defn _reverse [] ())
+(defn _memoize [] ())
+(defn _merge [] ())
+(defn _min [] ())
+(defn _max [] ())
+
+
+; (defn first [] ())
+; (defn nth [] ())
+; (defn peek [] ())
+; (defn pop [] ())
+; (defn first [] ())
+; (defn assoc [] ())
+; (defn assoc-in [] ())
+; (defn subvec [] ())
+; (defn first [] ())
+; (defn first [] ())
+; (defn first [] ())
+; (defn first [] ())
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
